7ffbad2f39e7e42598310a0935c9c78bcdf3c07d
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-04-03 02:06:00 +0900

    Title: 포탑 부수기, Time: 136ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240403/포탑 부수기/destroy-the-turret.py
+++ 240403/포탑 부수기/destroy-the-turret.py
@@ -1,84 +1,88 @@
 N, M, K = map(int,input().split())
-arr = [list(map(int,input().split())) for _ in range(N)]
+arr =[list(map(int,input().split())) for _ in range(N)]
 turn = [[0] * M for _ in range(N)]
 
 from collections import deque
 def bfs(si,sj,ei,ej):
     q = deque()
-    v = [[[] for _ in range(M)] for _ in range(N)] # 방문 경로
-    v[si][sj] = (si,sj)
     q.append((si,sj))
-    d = arr[si][sj] # 데미지
+    v = [[[] for _ in range(M)] for _ in range(N)]
+    v[si][sj] = (si,sj)
+    d = arr[si][sj]
+
     while q:
         ci,cj = q.popleft()
-        if (ci,cj) == (ei,ej): # 종료 조건
+        if (ci,cj) == (ei,ej):
             arr[ei][ej] = max(0,arr[ei][ej]-d)
             while True:
                 ci,cj = v[ci][cj]
                 if (ci,cj) == (si,sj):
                     return True
-                fset.add((ci,cj))
                 arr[ci][cj] = max(0,arr[ci][cj]-d//2)
-        for di, dj in ((0,1),(1,0),(0,-1),(-1,0)):
-            ni,nj = (ci+di)%N, (cj+dj)%M
-            if len(v[ni][nj])==0 and arr[ni][nj] >0: # 방문한적 없고, 벽이 아니면
+                fset.add((ci,cj))
+
+        for di,dj in ((0,1),(1,0),(0,-1),(-1,0)):
+            ni, nj = (ci + di)%N,  (cj + dj)%M
+            if not v[ni][nj] and arr[ni][nj] >0:
                 q.append((ni,nj))
-                v[ni][nj] = (ci,cj)
+                v[ni][nj] = (ci,cj) # 방문 기록을 남기기
     return False
 
 def bomb(si,sj,ei,ej):
-    d = arr[si][sj] # 데미지
-    arr[ei][ej] = max(0, arr[ei][ej]-d)
-
-    for di, dj in ((-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)):
-        ni, nj = (ei+di)%N ,(ej+dj)%M
+    d = arr[si][sj]
+    arr[ei][ej] = max(0,arr[ei][ej]-d)
+    for di,dj in ((0,-1),(-1,-1),(-1,0),(-1,1),(0,1),(1,1),(1,0),(1,-1)):
+        ni,nj = (ei+di)%N, (ej+dj)%M
         if (ni,nj) != (si,sj):
-            arr[ni][nj] = max(0, arr[ni][nj]-d//2)
-            fset.add((ni,nj))
+            arr[ni][nj] = max(0,arr[ni][nj]-d//2)
 
-for T in range(1,K+1):
-    # 완전탐색
+
+for T in range(K):
     # [1] 공격자 선정
-    mn, mx_turn, si, sj = 5001, 0, -1, -1
+    mn, mx_turn, si,sj = 5001, 0, -1, -1
+    for i in range(N):
+        for j in range(M):
+            if arr[i][j] <= 0:
+                continue
+            # 공격력 가장 낮은, 가장 최근에 공격한 포탑, 행과 열의 합이 가장 큰, 열 값이 가장 큰
+            if arr[i][j] < mn or (arr[i][j]==mn and mx_turn<turn[i][j]) or \
+                (arr[i][j] == mn and mx_turn==turn[i][j] and si+sj < i+j) or \
+                (arr[i][j] == mn and mx_turn==turn[i][j] and si+sj ==  i+j and sj < j):
+                mn , mx_turn, si, sj = arr[i][j], turn[i][j], i, j
+
+    # [2] 공격 대상자 선정
+    mx, mn_turn, ei,ej = -1, 1001, 11, 11
     for i in range(N):
         for j in range(M):
             if arr[i][j] <= 0: continue
-            #  [1-1] 공격력이 가장 낮은, 가장 최근에 공격한 포탑, 행과 열의 합이 가장 큰, 열 값이 가장 큰
-            if arr[i][j] < mn or (arr[i][j] == mn and mx_turn < turn[i][j]) or \
-                (arr[i][j] == mn and mx_turn == turn[i][j] and si+sj < i+j) or \
-                (arr[i][j] == mn and mx_turn == turn[i][j] and si+sj == i+j and sj<j):
-                mn, mx_turn, si,sj = arr[i][j],turn[i][j],i,j
+            # 공격력 가장 높은, 가장 공격한지 오래된 포탑, 행과 열의 합이 가장 작은, 열 값이 가장 작은
+            if arr[i][j] > mx or (arr[i][j]==mx and mn_turn>turn[i][j]) or \
+                (arr[i][j] == mx and mn_turn==turn[i][j] and ei+ej > i+j) or \
+                (arr[i][j] == mx and mn_turn==turn[i][j] and ei+ej ==  i+j and ej > j):
+                mx , mn_turn, ei, ej = arr[i][j], turn[i][j], i, j
 
-    # [2] 공격 대상 선정
-    mx, mn_turn, ei, ej = 0, 1001, 11, 11
-    for i in range(N):
-        for j in range(M):
-            if arr[i][j] <= 0: continue
-            # [2-1] 공격력이 가장 높은, 마지막으로 공격한 포탑, 행과 열의 합이 가장 작은, 열 값이 가장 작은
-            if arr[i][j] > mx or (arr[i][j] == mx and mn_turn > turn[i][j]) or \
-                (arr[i][j] == mx and mn_turn == turn[i][j] and ei+ej > i+j) or \
-                (arr[i][j] == mx and mn_turn == turn[i][j] and ei+ej == i+j and ej >j):
-                mx, mn_turn, ei, ej = arr[i][j], turn[i][j], i, j
-
-    arr[si][sj] += (M+N)  # 공격자 공격력 증가
-    turn[si][sj] = T
-    fset = set()
+    # [3] 공격자 공격력 상승
+    arr[si][sj] += (N+M)
+    turn[si][sj] = T # 턴 기록
+    fset = set() # 공격과 무관했던 포탑을 기록하기 위해 fset 선언
     fset.add((si,sj))
     fset.add((ei,ej))
 
     if bfs(si,sj,ei,ej) == False:
-        bomb(si,sj,ei,ej)
+        bomb(si,sj,ei,ej) # 포탄 공격
 
-    # 포탑 정비 -> 무관한 포탑 공격력 1 증가
     for i in range(N):
         for j in range(M):
-            # if (i,j) in fset: continue
-            if arr[i][j] > 0 and (i,j) not in fset:
+            if (i,j) not in fset and arr[i][j] > 0:
                 arr[i][j] += 1
 
     cnt = M*N
-    for lst in arr:
-        cnt -= lst.count(0)
-    if cnt <=1: break
+    for i in range(N):
+        for j in range(M):
+            if arr[i][j] == 0:
+                cnt -=1
+    if cnt <= 1:
+        break
+
 
 print(max(map(max,arr)))
