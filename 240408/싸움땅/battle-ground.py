b434c93e3348a4b089420d7d5e33893f476795f5
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-04-08 17:27:04 +0900

    Title: 싸움땅, Time: 205ms, Memory: 25MB, Status: Passed - CodeTree

--- 240408/싸움땅/battle-ground.py
+++ 240408/싸움땅/battle-ground.py
@@ -7,7 +7,9 @@ for i in range(N):
     for j in range(N):
         if arr[i][j] > 0: # 총이면
             gun[i][j].append(arr[i][j])
-            arr[i][j] = 0
+
+arr = [[0] * N for _ in range(N)] # arr에 사람만 표시하기 위해 저장
+
 # player 정보 i, j, dir, power, gun, score
 for m in range(1,M+1):
     i,j,dir,power = map(int,input().split())
@@ -20,10 +22,10 @@ opp = {0:2, 1:3, 2:0, 3:1}
 def loser(num,ci,cj,cd,cp,cg,cs):
     for k in range(4):
         ni,nj = ci+di[(cd+k)%4], cj +dj[(cd+k)%4]
-        if 0<=ni<N and 0<=nj<N and arr[ni][nj]<=0:
+        if 0<=ni<N and 0<=nj<N and arr[ni][nj]==0:
             if gun[ni][nj]:
                 cg = max(gun[ni][nj])
-            idx = arr[ci][cj]
+                gun[ni][nj].remove(cg)
             arr[ni][nj] = num
             players[num] = [ni,nj,(cd+k)%4,cp,cg,cs]
             return
@@ -31,7 +33,7 @@ def loser(num,ci,cj,cd,cp,cg,cs):
 # M턴 동안 게임 시작
 for turn in range(1,K+1):
     # [1] 1~M 플레이어 순서로 한칸 이동
-    for idx in range(1,M+1):
+    for idx in players:
         ci,cj,cd,cp,cg,cs = players[idx]
         ni,nj = ci+di[cd],cj+dj[cd]
         # [1-1-1] 범위 밖이면 반대로 이동
@@ -42,16 +44,13 @@ for turn in range(1,K+1):
         arr[ci][cj] = 0
 
         # [2-1] 이동한 위치에 플레이어가 없는 경우
-        if arr[ni][nj] <= 0:
+        if arr[ni][nj]== 0:
             # [2-1-1] 총이 있는지
             if gun[ni][nj]:
                 mx = max(gun[ni][nj]) # 가장 강한 총
                 if cg < mx: # 내가 총이 있는지
                     if cg > 0:
                         gun[ni][nj].append(cg)
-                    gun[ni][nj].remove(mx)
-                    cg = mx
-                else: # 내가 총이 없다면
                     gun[ni][nj].remove(mx)
                     cg = mx
 
@@ -68,13 +67,12 @@ for turn in range(1,K+1):
             # [2-2-1] 플레이어 승리
             if (cg+cp) > (eg+ep) or (cp+cg == eg+ep and cp>ep):
                 cs += (cg+cp) - (eg+ep)
-                loser(enemy,ei,ej,ed,ep,0,es)
+                loser(enemy,ni,nj,ed,ep,0,es)
 
                 if cg < eg:
                     if cg > 0:
                         gun[ni][nj].append(cg)
                     cg = eg
-                    # arr[ci][cj] = 0
                     arr[ni][nj] = idx
                     players[idx] = [ni, nj, cd, cp, cg, cs]
 
@@ -86,7 +84,8 @@ for turn in range(1,K+1):
                     players[idx] = [ni, nj, cd, cp, cg, cs]
 
             # [2-2-2] 적 승리
-            elif (eg+ep) > (cg+cp) or (cp+cg == eg+ep and ep>cp):
+            # elif (eg+ep) > (cg+cp) or (cp+cg == eg+ep and ep>cp):
+            else:
                 es += (eg+ep) - (cg+cp)
                 loser(idx,ni,nj,cd,cp,0,cs)
 
@@ -94,14 +93,17 @@ for turn in range(1,K+1):
                     if eg > 0:
                         gun[ni][nj].append(eg)
                     eg = cg
-                    arr[ni][nj] = enemy
-                    players[enemy] = [ei, ej, ed, ep, eg, es]
+                    # arr[ni][nj] = enemy
+                    # players[enemy] = [ei, ej, ed, ep, eg, es]
 
 
                 else:
-                    gun[ni][nj].append(cg)
-                    arr[ni][nj] = enemy
-                    players[enemy] = [ei, ej, ed, ep, eg, es]
+                    if cg > 0:
+                        gun[ni][nj].append(cg)
+                    # arr[ni][nj] = enemy
+                    # players[enemy] = [ei, ej, ed, ep, eg, es]
+                arr[ni][nj] = enemy
+                players[enemy] = [ni, nj, ed, ep, eg, es]
 
-for i in range(1,M+1):
+for i in players:
     print(players[i][5],end=' ')
