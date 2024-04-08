a0e5b025a4c98db079c5fd6e9e757eb50004a2aa
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-04-08 21:34:09 +0900

    Title: 싸움땅, Time: 183ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240408/싸움땅/battle-ground.py
+++ 240408/싸움땅/battle-ground.py
@@ -1,109 +1,103 @@
 N,M,K = map(int,input().split())
-players = {}
 
 arr = [list(map(int,input().split())) for _ in range(N)]
+
 gun = [[[] for _ in range(N)] for _ in range(N)]
+
 for i in range(N):
     for j in range(N):
-        if arr[i][j] > 0: # 총이면
+        if arr[i][j] > 0:
             gun[i][j].append(arr[i][j])
+            arr[i][j] = 0
 
-arr = [[0] * N for _ in range(N)] # arr에 사람만 표시하기 위해 저장
+players = {}
 
-# player 정보 i, j, dir, power, gun, score
 for m in range(1,M+1):
     i,j,dir,power = map(int,input().split())
-    players[m] = [i-1,j-1,dir,power,0,0]
     arr[i-1][j-1] = m
+    players[m] = [i-1,j-1,dir,power,0,0]
 
 di,dj = [-1,0,1,0], [0,1,0,-1]
 opp = {0:2, 1:3, 2:0, 3:1}
 
-def loser(num,ci,cj,cd,cp,cg,cs):
+
+def leave(num,ci,cj,cd,cg,cp,cs):
     for k in range(4):
-        ni,nj = ci+di[(cd+k)%4], cj +dj[(cd+k)%4]
-        if 0<=ni<N and 0<=nj<N and arr[ni][nj]==0:
+        ni,nj = ci+di[(cd+k)%4], cj+dj[(cd+k)%4]
+        if 0<=ni<N and 0<=nj<N and arr[ni][nj] == 0:
             if gun[ni][nj]:
                 cg = max(gun[ni][nj])
                 gun[ni][nj].remove(cg)
             arr[ni][nj] = num
-            players[num] = [ni,nj,(cd+k)%4,cp,cg,cs]
+            players[num] = [ni,nj,(cd+k)%4,cg,cp,cs]
             return
 
-# M턴 동안 게임 시작
+
 for turn in range(1,K+1):
-    # [1] 1~M 플레이어 순서로 한칸 이동
-    for idx in players:
-        ci,cj,cd,cp,cg,cs = players[idx]
-        ni,nj = ci+di[cd],cj+dj[cd]
-        # [1-1-1] 범위 밖이면 반대로 이동
-        if 0 > ni or ni >= N or 0 > nj or nj >= N:
+    # [1] 1~P번 플레이어
+    for i in range(1,M+1):
+        ci,cj,cd,cp,cg,cs = players[i]
+        ni,nj = ci+di[cd], cj+dj[cd]
+
+        # 범위 밖이면 반대 방향
+        if not (0<=ni<N and 0<=nj<N):
             cd = opp[cd]
-            ni,nj = ci+di[cd],cj+dj[cd]
+            ni, nj = ci + di[cd], cj + dj[cd]
 
-        arr[ci][cj] = 0
+        arr[ci][cj] = 0 # 이동처리
 
-        # [2-1] 이동한 위치에 플레이어가 없는 경우
-        if arr[ni][nj]== 0:
-            # [2-1-1] 총이 있는지
-            if gun[ni][nj]:
-                mx = max(gun[ni][nj]) # 가장 강한 총
-                if cg < mx: # 내가 총이 있는지
+        # 이동한 곳에 플레이어가 없다면
+        if arr[ni][nj] == 0:
+            if gun[ni][nj]: # 총이 있다면
+                mx = max(gun[ni][nj])
+                if mx > cg:
                     if cg > 0:
                         gun[ni][nj].append(cg)
                     gun[ni][nj].remove(mx)
                     cg = mx
 
-            arr[ni][nj] = idx
-            players[idx] = [ni, nj, cd, cp, cg, cs]
+            arr[ni][nj] = i
+            players[i] = [ni,nj,cd,cp,cg,cs]
 
 
-
-        # [2-2] 이동한 위치에 적이 있음
         else:
             enemy = arr[ni][nj]
             ei,ej,ed,ep,eg,es = players[enemy]
 
-            # [2-2-1] 플레이어 승리
-            if (cg+cp) > (eg+ep) or (cp+cg == eg+ep and cp>ep):
-                cs += (cg+cp) - (eg+ep)
-                loser(enemy,ni,nj,ed,ep,0,es)
+            # 플레이어 승 적 패배
+            if (cp+cg) > (ep+eg) or ((cp+cg) == (ep+eg) and cp>ep):
+                cs += (cp+cg) - (ep+eg)
+                leave(enemy,ei,ej,ed,ep,0,es)
 
-                if cg < eg:
+                if cg < eg: # 상대방 총이 더 썌
                     if cg > 0:
                         gun[ni][nj].append(cg)
                     cg = eg
-                    arr[ni][nj] = idx
-                    players[idx] = [ni, nj, cd, cp, cg, cs]
 
-
-                else:
-                    if eg > 0:
+                else: # 내 총이 더 썌
+                    if eg >0:
                         gun[ni][nj].append(eg)
-                    arr[ni][nj] = idx
-                    players[idx] = [ni, nj, cd, cp, cg, cs]
 
-            # [2-2-2] 적 승리
-            # elif (eg+ep) > (cg+cp) or (cp+cg == eg+ep and ep>cp):
+                arr[ni][nj] =i
+                players[i] = [ni,nj,cd,cp,cg,cs]
+
+
             else:
-                es += (eg+ep) - (cg+cp)
-                loser(idx,ni,nj,cd,cp,0,cs)
+                es += (ep + eg) - (cp + cg)
+                leave(i, ni, nj, cd, cp, 0, cs)
 
-                if eg < cg:
+                if eg < cg:  # 내 총이 더 썌
                     if eg > 0:
                         gun[ni][nj].append(eg)
                     eg = cg
-                    # arr[ni][nj] = enemy
-                    # players[enemy] = [ei, ej, ed, ep, eg, es]
 
-
-                else:
+                else:  # 내 총이 더 썌
                     if cg > 0:
                         gun[ni][nj].append(cg)
-                    # arr[ni][nj] = enemy
-                    # players[enemy] = [ei, ej, ed, ep, eg, es]
+
                 arr[ni][nj] = enemy
                 players[enemy] = [ni, nj, ed, ep, eg, es]
 
+
 for i in players:
-    print(players[i][5],end=' ')
+    print(players[i][5], end=' ')
