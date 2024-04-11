eb626b4ee129e8ba5b4f0fe6945fe4ca0d1f7871
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-04-11 20:00:36 +0900

    Title: 코드트리 빵, Time: 232ms, Memory: 25MB, Status: Passed - CodeTree

--- 240411/코드트리 빵/codetree-mon-bread.py
+++ 240411/코드트리 빵/codetree-mon-bread.py
@@ -1,6 +1,8 @@
 N, M = map(int,input().split())
+# 벽으로 둘러싸기
 arr = [[1] * (N+2)] + [[1] + list(map(int,input().split())) + [1] for _ in range(N)] + [[1] * (N+2)]
 
+# 베이스 캠프 처리
 basecamp = set()
 
 for i in range(1,N+1):
@@ -10,63 +12,61 @@ for i in range(1,N+1):
             arr[i][j] = 0
 
 store = {}
-for m in range(1,M+1):
-    i,j = map(int,input().split())
-    store[m] = (i,j)
 
+for idx in range(1,M+1):
+    i,j = map(int,input().split())
+    store[idx] = (i,j)
 
 from collections import deque
+
 def find(si,sj,dests):
     q = deque()
     q.append((si,sj))
     v = [[False] * (N+2) for _ in range(N+2)]
     v[si][sj] = True
-    alst = []
+    tlst = []
     while q:
         nq = deque()
-        for ci, cj in q:
+        for ci,cj in q:
 
-            if (ci,cj) in dests:
-                alst.append((ci,cj))
+            if (ci,cj) in dests: # 목적지 도착
+                tlst.append((ci,cj))
 
             else:
-                for ni,nj in ((ci-1,cj),(ci+1,cj),(ci,cj-1),(ci,cj+1)):
+                for ni,nj in ((ci-1,cj),(ci,cj-1),(ci,cj+1),(ci+1,cj)):
                     if arr[ni][nj] == 0 and not v[ni][nj]:
                         nq.append((ni,nj))
                         v[ni][nj] = True
         q = nq
 
-        if alst:
-            return sorted(alst)[0]
-
-
+        if tlst:
+            return sorted(tlst)[0]
 
 def solve():
-    q = deque()
     time = 1
     arrived = [0] * (M+1)
+    q = deque()
+
     while q or time == 1:
         nq = deque()
         tlst = []
-        # [1] 격자에 있는 사람들 모두가 본인이 가고 싶은 편의점 방향을 향해서 1칸 움직이기
+        # [1] 격자에 있는 사람들 모두가 본인이 가고 싶은 편의점 방향을 향해 1칸 움직임 (상우하좌)
         for ci,cj,m in q:
             if arrived[m] == 0:
-                ni, nj = find(store[m][0], store[m][1], set(((ci - 1, cj), (ci + 1, cj), (ci, cj - 1), (ci, cj + 1))))
+                ni,nj = find(store[m][0],store[m][1],set(((ci-1,cj),(ci,cj+1),(ci+1,cj),(ci,cj-1))))
                 if (ni,nj) == store[m]:
                     tlst.append((ni,nj))
                     arrived[m] = time
                 else:
-                    if arr[ni][nj] == 0:
-                        nq.append((ni,nj,m))
-
+                    nq.append((ni,nj,m))
         q = nq
-        # [2] 편의점에 도착하면 해당 편의점 이동 불가 처리
+
+        # [2] 편의점에 도착했으면 더이상 지나갈 수 없게 만들기
         if tlst:
-            for ai,aj in tlst:
-                arr[ai][aj] = 1
+            for ti, tj in tlst:
+                arr[ti][tj] = 1
 
-
-        # [3] 본인 편의점에서 가장 가까운 베이스 캠프
+        # [3] time<=M이라면 자신이가 가고 싶은 편의점으로 이동
         if time <= M:
             si,sj = store[time]
             ei,ej = find(si,sj,basecamp)
