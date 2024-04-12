d86de2665916867423020b9ff54d1e3299b6e28b
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-04-13 01:00:37 +0900

    Title: 코드트리 빵, Time: 240ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240413/코드트리 빵/codetree-mon-bread.py
@@ -0,0 +1,78 @@
+N, M = map(int,input().split())
+arr = [[1] * (N+2)] + [[1] + list(map(int,input().split())) + [1] for _ in range(N)] + [[1] * (N+2)]
+basecamp = set()
+for i in range(1,N+1):
+    for j in range(1,N+1):
+        if arr[i][j] == 1:
+            basecamp.add((i,j))
+            arr[i][j] = 0
+store = {}
+
+for idx in range(1,M+1):
+    i,j = map(int,input().split())
+    store[idx] = (i,j)
+
+from collections import deque
+def find(si,sj,dests):
+    q = deque()
+    q.append((si,sj))
+    v = [[False] * (N+2) for _ in range(N+2)]
+    v[si][sj] = True
+    tlst = []
+    while q:
+        nq = deque()
+        for ci,cj in q:
+
+            if (ci,cj) in dests:
+                tlst.append((ci,cj))
+
+            else:
+                for ni, nj in ((ci-1,cj),(ci,cj-1),(ci,cj+1),(ci+1,cj)):
+                    if arr[ni][nj] ==0 and not v[ni][nj]:
+                        nq.append((ni,nj))
+                        v[ni][nj] = True
+
+        q = nq
+
+        if tlst:
+            return sorted(tlst)[0]
+
+def solve():
+    time = 1
+    arrived = [0] * (M+1)
+    q = deque()
+
+    while q or time ==1:
+        nq = deque()
+        tlst = []
+        # [1]
+        for ci,cj,m in q:
+            if arrived[m] == 0:
+                ni,nj = find(store[m][0],store[m][1], set(((ci-1,cj),(ci,cj-1),(ci,cj+1),(ci+1,cj))))
+                if (ni,nj) == store[m]:
+                    tlst.append((ni,nj))
+                    arrived[m] = time
+                else:
+                    nq.append((ni,nj,m))
+
+        q = nq
+        # [2]
+        if tlst:
+            for ti,tj in tlst:
+                arr[ti][tj] = 1
+
+        # [3]
+
+        if time <= M :
+            si,sj = store[time]
+            ei,ej = find(si,sj,basecamp)
+            basecamp.remove((ei,ej))
+            arr[ei][ej] = 1
+            q.append((ei,ej,time))
+
+        time += 1
+
+    return max(arrived)
+
+ans = solve()
+print(ans)
