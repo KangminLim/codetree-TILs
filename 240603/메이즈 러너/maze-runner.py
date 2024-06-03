e5b9b16d3d346b3e3340ad6d39f3cd5e7e415e74
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-06-03 19:05:27 +0900

    Title: 메이즈 러너, Time: 125ms, Memory: 25MB, Status: Passed - CodeTree

--- 240603/메이즈 러너/maze-runner.py
+++ 240603/메이즈 러너/maze-runner.py
@@ -2,11 +2,11 @@ N,M,K = map(int,input().split())
 arr = [list(map(int,input().split())) for _ in range(N)]
 for _ in range(M):
     si,sj = map(lambda x:int(x)-1, input().split())
-    arr[si][sj] = -1 # 초기 좌표
+    arr[si][sj] -= 1 # 초기 좌표
 ei,ej = map(lambda x:int(x)-1, input().split())
 arr[ei][ej] = -11
 ans = 0
-cnt = N
+cnt = M
 
 def find_square(arr):
     mn = N
@@ -15,6 +15,8 @@ def find_square(arr):
             if -11<arr[i][j]<0: # 참가자일 때
                 dist = max(abs(ei-i),abs(ej-j))
                 mn = min(mn,dist)
+
+
     for si in range(N-mn):
         for sj in range(N-mn):
             if si<=ei<=si+mn and sj<=ej<=sj+mn:
@@ -28,23 +30,23 @@ for turn in range(1,K+1):
     # 1. 모든 참가자가 동시에 이동
     for i in range(N):
         for j in range(N):
-            if -11<arr[i][j]<=-1: # 참가자일 때
+            if -11<arr[i][j]<0: # 참가자일 때
                 dist = abs(ei-i)+abs(ej-j)
                 for di,dj in ((-1,0),(1,0),(0,-1),(0,1)):
                     ni,nj = i+di,j+dj
                     new_dist = abs(ei - ni) + abs(ej - nj)
                     if 0<=ni<N and 0<=nj<N and arr[ni][nj] <= 0 and dist>new_dist: # 범위 내, 벽 x, 거리 가까워져야함
+                        ans += arr[i][j]
+                        narr[i][j] -= arr[i][j]
                         if (ni,nj) == (ei,ej): # 탈출
-                            ans += arr[i][j]
                             cnt += arr[i][j]
-                            narr[i][j] = 0 # 이동처리
                         else: # 이동
-                            ans += arr[i][j]
-                            narr[i][j] -= arr[i][j]
                             narr[ni][nj] += arr[i][j]
                         break
+
     arr = narr
-    if cnt == 0: break
+    if cnt == 0:
+        break
     # 2. 미로 회전
     si,sj,L = find_square(arr)
 
