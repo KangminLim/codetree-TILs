f3affdd21b62d6d5271de41df4cfdcd62a0c0645
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-08-28 20:16:33 +0900

    Title: 놀이기구 탑승, Time: 204ms, Memory: 25MB, Status: Passed - CodeTree

--- 240828/놀이기구 탑승/go-on-the-rides.py
+++ 240828/놀이기구 탑승/go-on-the-rides.py
@@ -12,9 +12,10 @@ for idx in range(n*n):
 
     # 1. 격자를 벗어나지 않은 4방향으로 인접한 칸 중 앉아있는 좋아하는 친구의 수가 가장 많은 위치로 간다.
     # 1.1 배치할 자리 찾기
-    fcnt, zcnt = 0,0
+    fcnt, zcnt, tcnt = 0,0,0
     tfi,tfj = n,n
     tzi,tzj = n,n
+    tlst = []
     for ci in range(n):
         for cj in range(n):
             tfcnt, tzcnt = 0, 0
@@ -23,25 +24,22 @@ for idx in range(n*n):
                 for ni, nj in ((ci-1,cj),(ci,cj+1),(ci+1,cj),(ci,cj-1)):
                     # 범위 내
                     if 0<=ni<n and 0<=nj<n:
-                        if arr[ni][nj] > 0 and arr[ni][nj] in fdict[idx]: # 좋아하는 친구가 있다면
+                        if arr[ni][nj] in fdict[idx]: # 좋아하는 친구가 있다면
                             tfcnt += 1
                         elif arr[ni][nj] == 0:
                             tzcnt += 1
-            if tfcnt > fcnt:
-                fcnt = max(tfcnt,fcnt)
-                tfi,tfj = ci,cj
-            if tzcnt > zcnt:
-                zcnt = max(tzcnt,zcnt)
-                tzi,tzj = ci,cj
-    if fcnt > 0:
-        arr[tfi][tfj] = fdict[idx][0]
-    elif fcnt == 0 and zcnt > 0:
-        arr[tzi][tzj] = fdict[idx][0]
-    if idx == n**2-1:
+                tlst.append((tfcnt,tzcnt,ci,cj))
+
+    tlst.sort(key = lambda x:(-x[0],-x[1],x[2],x[3]))
+    if tlst:
+        ti, tj = tlst[0][2], tlst[0][3]
+        arr[ti][tj] = fdict[idx][0]
+    else:
         for i in range(n):
             for j in range(n):
                 if arr[i][j] == 0:
                     arr[i][j] = fdict[idx][0]
+
 
 ans = 0
 for ci in range(n):
