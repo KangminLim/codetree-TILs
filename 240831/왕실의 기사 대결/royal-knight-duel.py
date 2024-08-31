40c0b629b9bfdb1919f29a513e7cf656010e09d6
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-08-31 20:31:24 +0900

    Title: 왕실의 기사 대결, Time: 277ms, Memory: 25MB, Status: Passed - CodeTree

--- 240831/왕실의 기사 대결/royal-knight-duel.py
+++ 240831/왕실의 기사 대결/royal-knight-duel.py
@@ -1,63 +1,59 @@
 L, N, Q = map(int,input().split())
 arr = [[2] * (L+2)] + [[2] + list(map(int,input().split())) + [2] for _ in range(L)] + [[2] * (L+2)]
-knight = {}
+knights = {}
 init_k = [0] * (N+1)
-
 for idx in range(1,N+1):
-    i,j,h,w,k = map(int,input().split())
-    knight[idx] = [i,j,h,w,k]
+    r,c,h,w,k = map(int,input().split())
+    knights[idx] = [r,c,h,w,k]
     init_k[idx] = k
-
 di,dj = [-1,0,1,0], [0,1,0,-1]
-
 from collections import deque
-def push_knight(start,dr):
+def bfs(start, cd):
     q = deque()
     q.append(start)
     damage = [0] * (N+1)
+    # 연쇄 처리를 위한 fset 추가
     fset = set()
     fset.add(start)
     while q:
         cur = q.popleft()
-        ci,cj,ch,cw,ck = knight[cur]
-        ni,nj = ci+di[dr],cj+dj[dr]
-
+        ci,cj,ch,cw,ck = knights[cur]
+        ni,nj = ci+di[cd], cj+dj[cd]
+        # 1. 2중 for문(데미지 및 벽 처리)
         for i in range(ni,ni+ch):
             for j in range(nj,nj+cw):
-                if arr[i][j] == 1: # 함정 개수만큼 +1
-                    damage[cur] += 1
-                elif arr[i][j] == 2: # 벽을 만나면 이동 취소
+                if arr[i][j] == 2:
                     return
-
-        # 기사가 이동하려는 위치에 다른 기사가 있다면 그 기사도 이동시킴
-        for i in knight:
-            if i not in fset:
-                ti,tj,th,tw,tk = knight[i]
-                if ni <= ti <= ni+ch-1 or nj <= tj <= nj+cw-1 or ti <= ni <= ti+th-1 or tj <= nj <= tj+tw-1:
-                    q.append(i)
-                    fset.add(i)
-                # if ni<=ti+th-1 and nj<=tj+tw-1 and ti <= ni+ch-1 and tj <= nj+cw-1:
-                #     q.append(i)
-                #     fset.add(i)
-
+                elif arr[i][j] == 1:
+                    damage[cur] += 1
+        # 2. 이동 후 모든 knight 순회
+        for idx in knights:
+            if idx not in fset:
+                ti,tj,th,tw,tk = knights[idx]
+                # if ni <= ti + th - 1 and nj <= tj + tw - 1 and ti <= ni + ch - 1 and tj <= nj + cw - 1:
+                #     q.append(idx)
+                #     fset.add(idx)
+                # if ni <= ti+th-1 and nj <= tj+tw-1 and ti <= ni+ch-1 and tj <= nj+cw-1:
+                #     q.append(idx)
+                #     fset.add(idx)
+                if (ni <= ti <= ni + ch - 1 or ti <= ni <= ti + th - 1) and (tj <= nj <= tj + tw - 1 or nj <= tj <= nj + cw - 1 ):
+                    q.append(idx)
+                    fset.add(idx)
     damage[start] = 0
-    for idx in range(1,N+1):
-        if idx in fset:
-            ci,cj,ch,cw,ck = knight[idx]
-            if ck - damage[idx] >0:
-                knight[idx] = [ci+di[dr],cj+dj[dr],ch,cw,ck-damage[idx]]
-            else:
-                knight.pop(idx)
+    for idx in fset:
+        ci,cj,ch,cw,ck = knights[idx]
+        if ck - damage[idx] <= 0:
+            knights.pop(idx)
+        else:
+            knights[idx] = [ci+di[cd],cj+dj[cd],ch,cw,ck-damage[idx]]
 
-
-for _ in range(Q):
+for turn in range(Q):
     idx, dr = map(int,input().split())
-    if idx in knight:
-        push_knight(idx,dr)
+    if idx in knights:
+        bfs(idx,dr)
 
 ans = 0
-for i in range(1,N+1):
-    if i in knight:
-        ans += init_k[i] - knight[i][4]
+for idx in knights:
+    ans += init_k[idx] - knights[idx][4]
 
 print(ans)
