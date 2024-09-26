9542c5ccd17ed094a49840ed4277a41c98f3e3ef
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-09-26 19:01:05 +0900

    Title: 팩맨, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240926/팩맨/pacman.py
+++ 240926/팩맨/pacman.py
@@ -5,7 +5,6 @@ di,dj = [-1,-1,0,1,1,1,0,-1],[0,-1,-1,-1,0,1,1,1]
 arr = [[0] * 4 for _ in range(4)]
 tdi,tdj = [-1,0,1,0],[0,-1,0,1]
 die = [[0] * 4 for _ in range(4)]
-
 for _ in range(M):
     si,sj,dr = map(int,input().split())
     mlst.append((si-1,sj-1,dr-1))
@@ -20,7 +19,6 @@ for turn in range(1,T+1):
     # 1. 몬스터 복제 시도
     tlst = [x[:] for x in mlst]
     narr = [x[:] for x in arr]
-    # print('')
 
     # 2. 몬스터 이동
     for i in range(len(mlst)):
@@ -37,32 +35,29 @@ for turn in range(1,T+1):
     # 3. 팩맨 이동 위치 찾기
     mx = 0
     dlst = []
-    # print('')
 
     # 이동 과정 3회 (상,좌,하,우), 이동 과정에서 먹는 몬스터 mx, dr, 값 저장
     tpi,tpj = pi, pj
     for i in range(4):
         fni,fnj = tpi+tdi[i], tpj+tdj[i]
-        tmp1 = 0
         if 0<=fni<4 and 0<=fnj<4:
             tmp1 = arr[fni][fnj]
             for j in range(4):
                 tmp2 = 0
                 sni,snj = fni + tdi[j], fnj + tdj[j]
                 if 0 <= sni < 4 and 0 <= snj < 4:
-                    if (fni, fnj) != (sni, snj):
+                    if (fni, fnj) != (sni, snj) and (fni, fnj) != (tpi, tpj):
                         tmp2 = arr[sni][snj]
                     for k in range(4):
                         tmp3 = 0
                         tni,tnj = sni+tdi[k],snj+tdj[k]
                         if 0 <= tni < 4 and 0 <= tnj < 4:
-                            if (tni,tnj) != (sni,snj) and (tni,tnj) != (fni,fnj):
+                            if (tni,tnj) != (sni,snj) and (tni,tnj) != (fni,fnj) and (tni, tnj) != (tpi, tpj):
                                 tmp3 = arr[tni][tnj]
                             tmp = tmp1+tmp2+tmp3
                             if tmp > mx:
                                 mx = tmp
                                 dlst.append((i,j,k))
-    # print('')
 
     # 4. 팩맨 이동
     if mx > 0:
@@ -72,33 +67,47 @@ for turn in range(1,T+1):
         tmp1 = arr[fni][fnj]
         if tmp1 > 0:
             arr[fni][fnj] = 0
-            die[fni][fnj] = -2
+            die[fni][fnj] = -3
         sni, snj = fni + tdi[j], fnj + tdj[j]
         tmp2 = arr[sni][snj]
 
         if tmp2 > 0:
             arr[sni][snj] = 0
-            die[sni][snj] = -2
+            die[sni][snj] = -3
         tni, tnj = sni + tdi[k], snj + tdj[k]
         tmp3 = arr[tni][tnj]
         if tmp3 > 0:
             arr[tni][tnj] = 0
-            die[tni][tnj] = -2
+            die[tni][tnj] = -3
         # 팩맨 이동 처리
-        arr[pi][pj] = 0
         pi,pj = tni, tnj
-        arr[pi][pj] = -1
         for mi,mj,md in mlst:
             if (mi,mj) != (fni,fnj) and (mi,mj) != (sni,snj) and (mi,mj) != (tni,tnj):
                 slst.append((mi,mj,md))
-    # print('')
+    else:
+        tpi,tpj = pi, pj
+        for i in range(4):
+            fni, fnj = tpi + tdi[i], tpj + tdj[i]
+            if 0 <= fni < 4 and 0 <= fnj < 4:
+                for j in range(4):
+                    sni, snj = fni + tdi[j], fnj + tdj[j]
+                    if 0 <= sni < 4 and 0 <= snj < 4:
+                        for k in range(4):
+                            tni, tnj = sni + tdi[k], snj + tdj[k]
+                            if 0 <= tni < 4 and 0 <= tnj < 4:
+                                pi, pj = tni, tnj
+                                flag = True
+                                break
+                    if flag:
+                        break
+            if flag:
+                break
 
 
     # 5. 몬스터 복제 완성
     for ti,tj,td in tlst:
         arr[ti][tj] += 1
     mlst = slst + tlst
-    # print('')
 
 ans = 0
 for i in range(4):
