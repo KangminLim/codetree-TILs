d07a8b727bfc4891e14627050ceb79adce011e9c
KangMin <87487729+KangminLim@users.noreply.github.com>
2024-10-04 12:01:47 +0900

    Title: 싸움땅, Time: 137ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 241004/싸움땅/battle-ground.py
@@ -0,0 +1,99 @@
+N,M,K = map(int,input().split())
+arr = [list(map(int,input().split())) for _ in range(N)]
+gun = [[[] for _ in range(N)] for _ in range(N)]
+
+for i in range(N):
+    for j in range(N):
+        if arr[i][j] > 0:
+            gun[i][j].append(arr[i][j])
+            arr[i][j] = 0
+
+players = {}
+
+for idx in range(1,M+1):
+    si,sj,dr,power = map(int,input().split())
+    arr[si-1][sj-1] = idx
+    # ci,cj,cd,cp,cg,cs
+    players[idx] = [si-1,sj-1,dr,power,0,0]
+
+di,dj = [-1,0,1,0],[0,1,0,-1]
+opp = {0:2,1:3,2:0,3:1}
+
+def lose(idx,ci,cj,cd,cp,cg,cs):
+    for k in range(4):
+        ni,nj = ci+di[(cd+k)%4], cj+dj[(cd+k)%4]
+        if 0<=ni<N and 0<=nj<N and arr[ni][nj]==0:
+            if gun[ni][nj]:
+                cg = max(gun[ni][nj])
+                gun[ni][nj].remove(cg)
+            arr[ni][nj] = idx
+            players[idx] = [ni,nj,(cd+k)%4,cp,cg,cs]
+            return
+
+for turn in range(1,K+1):
+
+    # 1. 모든 플레이어 이동
+    for idx in range(1,M+1):
+        ci,cj,cd,cp,cg,cs = players[idx]
+        ni,nj = ci+di[cd], cj+dj[cd]
+        if not (0<=ni<N and 0<=nj<N):
+            cd = opp[cd]
+            ni, nj = ci + di[cd], cj + dj[cd]
+
+        # 이동 처리
+        arr[ci][cj] = 0
+
+        # 2.1 플레이어가 없다면
+        if arr[ni][nj] == 0:
+            if gun[ni][nj]:
+                mx = max(gun[ni][nj])
+                if mx > cg:
+                    if cg > 0:
+                        gun[ni][nj].append(cg)
+                    gun[ni][nj].remove(mx)
+                    cg = mx
+                else:
+                    cg = mx
+                    gun[ni][nj].remove(mx)
+            players[idx] = [ni,nj,cd,cp,cg,cs]
+            arr[ni][nj] = idx
+
+            # else:
+            #     players[idx] = [ni,nj,cd,cp,cg,cs]
+            #     arr[ni][nj] = idx
+
+
+
+        # 2.2 플레이어가 있다면
+        else:
+            e = arr[ni][nj]
+            ei,ej,ed,ep,eg,es = players[e]
+            # 2.2.1 플레이어 승리
+            if (cp+cg) > (ep+eg) or ((cp+cg) == (ep+eg) and cp > ep):
+                cs += (cp+cg) - (ep+eg)
+                if eg > cg:
+                    if cg > 0:
+                        gun[ni][nj].append(cg)
+                    cg = eg
+                else:
+                    gun[ni][nj].append(eg)
+                arr[ni][nj] = idx
+                players[idx] = [ni,nj,cd,cp,cg,cs]
+                lose(e,ei,ej,ed,ep,0,es)
+
+            else:
+                es += (ep+eg) - (cp+cg)
+                if cg > eg:
+                    if eg > 0:
+                        gun[ni][nj].append(eg)
+                    eg = cg
+                else:
+                    gun[ni][nj].append(cg)
+                arr[ni][nj] = e
+                players[e] = [ei,ej,ed,ep,eg,es]
+                lose(idx,ni,nj,cd,cp,0,cs)
+
+    # print('')
+
+for i in range(1,M+1):
+    print(players[i][5],end=' ')
